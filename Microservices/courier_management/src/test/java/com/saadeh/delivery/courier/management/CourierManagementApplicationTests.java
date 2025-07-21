fa5bd3f7546d6fd843f372aba0c783f4717594a0
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-07-21 16:08:49 -0300

    Project created

---
+++ Microservices/courier_management/src/test/java/com/saadeh/delivery/courier/management/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadeh.delivery.courier.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
